28df770f8db3c56beb52d57a0c10fd1c409fe266
Ethan Cruz <empcruz11@gmail.com>
2025-02-07 05:00:53 +0800

    user_db changes and landing page changes

--- myproject/static/landing.js
+++ myproject/static/landing.js
@@ -52,47 +52,40 @@ let slideIndex = 0;
           const dentistPopupText = document.getElementById('dentist-popup-text');
   
           const dentistData = {
-              dr_peter: {
-                  image: "D:/Acerld/pictures/Sample.jpg",
-                  description: 
-                    `<p>A graduate of doctor of dental medicine in Our Lady of Fatima University in 2003. </p>
-                  
-                  <p>She was awarded as the “scholar of the year” for winning a dental interschool competition given by the Southern California Filipino Dental Society held in Philippine Dental Association Annual convention in 2002. </p>
-                  
-                  <p>In 2004-2011, she became part of Spectrumed Incorporated  as sales manager to division head of dental department. </p>
-                  
-                  <p>In 2004 she has given the chance to pursue her studies in Oral Implantology from International Research and Development Institute, Tel-aviv, Israel</p>
-                  
-                  <p>In 2006-2007, she completed her post-graduate course of Oral Implantology in International Center for Dental Education Research and Development Institute, Alpha-Bio, Philippines. </p>
-                  
-                  <p>In 2008-2009, she further studied a post-graduate preceptorship in dental implantology under Nobel Biocare, Philippines. </p>
-                  
-                  <p>In 2009, she was sponsored a  training in GUIDED IMPLANT Surgery using Nobel Guide under Dr. Jasmes Chow in Hong Kong. </p>
-                  <p>In the same year 2009, she was recognized as the EMPLOYEE OF THE YEAR by SPECTRUMED Inc. </p>
-                  
-                  <p>In 2011-2014, she migrated to Singapore as Clinical Sales Executive of Nobel Biocare and has awarded as Top Sales Silver Awardee in Southeast Asia in 2013. </p>
-                  
-                  <p>In 2015-2019, she has  worked with 2 companies as Business Development Manager in Apexmed  and Clinical consultant in Spectrumed</p>
-                  
-                  <p>In 2015, she studied a preceptorship in Orthodontics under Dr. Enrico Dolatre. </p>
-                  
-                  <p>In 2016, she has achieved her official license of Photon Induced Photoaccoustic Streaming Technology (Dental L:aser)  given by Laser and Health in Slovenia, Europe. </p>
-                  
-                  <p>In 2016-2018, she completed her post-graduate course in Biofunctional Dentistry which involves teeth-muscles and TMJ. </p>
-                  
-                  <p>Last year, she started  studying Orofacial pain and Occlusion under Dr. Elaine Rozul, who studied in University of Louisiana and Dawson Academy in USA. </p>
-                  
-                  <p>Currently, practicing in Acerld Dental Care as the owner and an active member of Philippine Academy of Implant Dentistry and Philippine College of Oral Implantologist.-Dr. Peter Lou Santiago-Caronan</p>`
-              },
-              dr_brian: {
-                  image: "D:/Acerld/pictures/Sample.jpg",
-                  description: "<p>Dr. Brian S. Bongolan is a newly licensed dentist (as of the document date) eager to learn and improve his skills to deliver quality dental treatment. His skills include patient management, oral prophylaxis, tooth restoration, tooth extraction, and prosthodontic treatment. He has experience as an Associate Dentist at Zen Smiles Dental Clinic (July 2023 - December 2024), Acerld Dental Care (October 2023 - Present), and AllSmiles Dental Center (May 2024 - Present). Dr. Bongolan is a member of the Philippine Dental Association (Pasig-Pateros-Taguig Chapter and Filipino Chinese Dental Foundation Inc.), and the Philippine Academy of Esthetic Dentistry. He served as Project Manager (2017-2020) and Vice President, Central Region (2020-2021) for the Philippine Dental Students Association - National Executive Board. He also worked as a Dental Clinician at Centro Escolar University - Makati (2019-2020). Dr. Bongolan received his Doctor of Dental Medicine degree from Centro Escolar University - Makati (2015-2021), secondary education at La Consolacion College - Pasig (2010-2013) and Lourdes School of Mandaluyong (2013-2015). He has also received training from the Philippine Academy Of Esthetic Dentistry (January 2024 - May 2024) and Prime Orthodontics Studies and Training (October - Present).</p>" // Add description
-              },
-              dr_chloie: {
-                  image: "D:/Acerld/pictures/Sample.jpg",
-                  description: '<p>Dr. Chloie Jeremy R. Tuason is a board-certified dentist (November 2024) committed to ongoing professional development.  Proficient in preventive treatments, Dr. Tuason is adaptable to both solo and team settings and is fluent in English and Filipino. Her education includes a Doctor of Dental Medicine degree from Centro Escolar University - Manila (2017-2023) and an Associate in Arts in Pre-Dentistry from the same university (2013-2017).  From June 2017 to December 2023, she worked as a Dental Clinician at Centro Escolar University - Manila, independently performing various dental procedures on live patients in a university setting.  Dr. Tuason has attended seminars on current trends in aesthetic and restorative dentistry (Dr. Maricar Joy Andres, 2019), orthodontics (Dr. Romeo Jacobs), and oral surgery (Dr. Mendelssohn Manalaysay).</p>'
-              }
-          };
+            dr_peter: {
+                image: dentistImages.dr_peter,
+                description: 
+                  `<p>A graduate of doctor of dental medicine in Our Lady of Fatima University in 2003. </p>
+                
+                <p>She was awarded as the “scholar of the year” for winning a dental interschool competition given by the Southern California Filipino Dental Society held in Philippine Dental Association Annual convention in 2002. </p>
+                
+                <p>In 2004-2011, she became part of Spectrumed Incorporated  as sales manager to division head of dental department. </p>
+                
+                <p>In 2004 she has given the chance to pursue her studies in Oral Implantology from International Research and Development Institute, Tel-aviv, Israel</p>
+                
+                <p>In 2006-2007, she completed her post-graduate course of Oral Implantology in International Center for Dental Education Research and Development Institute, Alpha-Bio, Philippines. </p>
+                
+                <p>In 2008-2009, she further studied a post-graduate preceptorship in dental implantology under Nobel Biocare, Philippines. </p>
+                
+                <p>In 2009, she was sponsored a  training in GUIDED IMPLANT Surgery using Nobel Guide under Dr. Jasmes Chow in Hong Kong. </p>
+                <p>In the same year 2009, she was recognized as the EMPLOYEE OF THE YEAR by SPECTRUMED Inc. </p>
+                
+                <p>In 2011-2014, she migrated to Singapore as Clinical Sales Executive of Nobel Biocare and has awarded as Top Sales Silver Awardee in Southeast Asia in 2013. </p>
+                
+                <p>In 2015-2019, she has  worked with 2 companies as Business Development Manager in Apexmed  and Clinical consultant in Spectrumed</p>
+                
+                <p>In 2015, she studied a preceptorship in Orthodontics under Dr. Enrico Dolatre. </p>
+                
+                <p>In 2016, she has achieved her official license of Photon Induced Photoaccoustic Streaming Technology (Dental L:aser)  given by Laser and Health in Slovenia, Europe. </p>
+                
+                <p>In 2016-2018, she completed her post-graduate course in Biofunctional Dentistry which involves teeth-muscles and TMJ. </p>
+                
+                <p>Last year, she started  studying Orofacial pain and Occlusion under Dr. Elaine Rozul, who studied in University of Louisiana and Dawson Academy in USA. </p>
+                
+                <p>Currently, practicing in Acerld Dental Care as the owner and an active member of Philippine Academy of Implant Dentistry and Philippine College of Oral Implantologist.-Dr. Peter Lou Santiago-Caronan</p>`
+            },
+          
+        };
   
           dentists.forEach(dentist => {
               dentist.addEventListener('click', () => {
