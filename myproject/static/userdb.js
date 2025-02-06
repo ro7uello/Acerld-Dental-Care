e05f015b8197cb2a565642691a12857e019053ca
Ethan Cruz <empcruz11@gmail.com>
2025-02-07 05:08:53 +0800

    user_db changess

--- myproject/static/userdb.js
+++ myproject/static/userdb.js
@@ -92,34 +92,3 @@ document.addEventListener('DOMContentLoaded', function() {
     });
 });
 
-let slideIndex = 0;
-        
-            function showSlides() {
-              let slides = document.getElementsByClassName("slides");
-              let dots = document.getElementsByClassName("dot");
-        
-              for (let i = 0; i < slides.length; i++) {
-                slides[i].style.display = "none";
-              }
-                
-              slideIndex++;
-              if (slideIndex > slides.length) {
-                slideIndex = 1;
-              }
-        
-              slides[slideIndex - 1].style.display = "block";
-        
-              for (let i = 0; i < dots.length; i++) {
-                dots[i].classList.remove("active");
-              }
-        
-              dots[slideIndex - 1].classList.add("active");
-              setTimeout(showSlides, 3000);
-            }
-        
-            showSlides();
-        
-            function currentSlide(n) {
-              slideIndex = n - 1;
-              showSlides();
-            }
